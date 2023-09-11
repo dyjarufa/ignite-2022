a8e0230a345674d6d0dbb6f2b2dd71a7b652f771
Jady Rufino <jadyrufa@gmail.com>
2023-09-11 08:39:02 +0100

    feat: add component controller

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -17,8 +17,13 @@ import {
 import { ArrowRight } from 'phosphor-react'
 
 import { z } from 'zod'
-import { useFieldArray, useForm } from 'react-hook-form'
 import { getWeekdays } from '@/utils/get-week-days'
+
+/*
+ * Controller ou Componentes controlados: Se você estiver usando componentes controlados ou componentes de terceiros que não expõem um ref
+ * (por exemplo, muitos componentes de UI de bibliotecas populares), você precisará do Controller para integrá-los ao React Hook Form.
+ */
+import { Controller, useFieldArray, useForm } from 'react-hook-form'
 
 export default function TimeIntervals() {
   const timeIntervalsFormSchema = z.object({})
@@ -27,6 +32,7 @@ export default function TimeIntervals() {
     register,
     handleSubmit,
     control,
+    watch,
     formState: { errors, isSubmitting },
   } = useForm({
     defaultValues: {
@@ -35,11 +41,14 @@ export default function TimeIntervals() {
         { weekDay: 1, enable: true, startTime: '08:00', endTime: '06:00' },
         { weekDay: 2, enable: true, startTime: '08:00', endTime: '06:00' },
         { weekDay: 3, enable: true, startTime: '08:00', endTime: '06:00' },
-        { weekDay: 5, enable: true, startTime: '08:00', endTime: '06:00' },
-        { weekDay: 6, enable: true, startTime: '08:00', endTime: '06:00' },
+        { weekDay: 5, enable: false, startTime: '08:00', endTime: '06:00' },
+        { weekDay: 6, enable: false, startTime: '08:00', endTime: '06:00' },
       ],
     },
   })
+
+  //* Permite saber em tempo real as mudanças que sofreram em um campo
+  const intervals = watch('intervals')
 
   const weekDays = getWeekdays()
 
@@ -66,7 +75,26 @@ export default function TimeIntervals() {
             return (
               <IntervalItem key={field.id}>
                 <IntervalDay>
-                  <Checkbox />
+                  {/* ..register não irá funcionar no Checkbox pois ele não é um elemento nativo do html */}
+
+                  {/* Controller ->  quando tem um elemento em tela visual que insere uma informação no formulário
+                  e não é um elemento nativo html
+                  */}
+                  <Controller
+                    name={`intervals.${index}.enable`}
+                    control={control}
+                    render={({ field }) => {
+                      return (
+                        <Checkbox
+                          onCheckedChange={(checked) => {
+                            field.onChange(checked === true) //? confirmando que é um valor true, pois o type do checked pode ser 'indeterminate'
+                          }}
+                          checked={field.value}
+                        />
+                      )
+                    }}
+                  />
+
                   <Text>{weekDays[field.weekDay]}</Text>
                 </IntervalDay>
                 <IntervalInputs>
@@ -74,14 +102,15 @@ export default function TimeIntervals() {
                     size="sm"
                     type="time"
                     step={60}
+                    disabled={intervals[index].enable === false}
                     {...register(`intervals.${index}.startTime`)}
                   />
-                </IntervalInputs>
-                <IntervalInputs>
+
                   <TextInput
                     size="sm"
                     type="time"
                     step={60}
+                    disabled={intervals[index].enable === false}
                     {...register(`intervals.${index}.endTime`)}
                   />
                 </IntervalInputs>
